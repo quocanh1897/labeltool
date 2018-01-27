d5c38c4ae807c8421dea4d88dcd11c654ef0471a
Trung Trinh <trinhtrung96@gmail.com>
2018-01-28 01:56:42 +0700

    Adhere to eslint

--- src/utils.js
+++ src/utils.js
@@ -1,46 +1,52 @@
 export function getSelectionText() {
-    var selObj = window.getSelection();
-    try {
-        var selRange = selObj.getRangeAt(0);
-    } catch (err) {
-        return null
-    }
-    // do stuff with the range
-    console.log(selRange)
-    return selRange
+  // eslint-disable-next-line
+  const selObj = window.getSelection();
+  try {
+    const selRange = selObj.getRangeAt(0);
+    return selRange;
+  } catch (err) {
+    return null;
+  }
 }
 
 export function trimNewLine(x) {
-    return x.replace(/^[\r\n]+|[\r\n]+$/gm, '');
+  return x.replace(/^[\r\n]+|[\r\n]+$/gm, '');
 }
 
 export function generateRandomString(N) {
-    const s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    return Array(N).join().split(',').map(function () { return s.charAt(Math.floor(Math.random() * s.length)); }).join('');
+  const s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  return Array(N).join().split(',').map(() => s.charAt(Math.floor(Math.random() * s.length)))
+    .join('');
 }
 
 export function checkParentRelation(parentNode, childNode) {
-    if ('contains' in parentNode) {
-        return parentNode.contains(childNode);
-    }
-    else {
-        return parentNode.compareDocumentPosition(childNode) % 16;
-    }
+  if ('contains' in parentNode) {
+    return parentNode.contains(childNode);
+  }
+  return parentNode.compareDocumentPosition(childNode) % 16;
 }
 export function download(data, filename, type) {
-    var file = new Blob([data], { type: type });
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    else { // Others
-        var a = document.createElement("a"),
-            url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function () {
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);
-        }, 0);
-    }
+  // eslint-disable-next-line
+  const file = new Blob([data], { type });
+  // eslint-disable-next-line
+  if (window.navigator.msSaveOrOpenBlob) // IE10+
+    // eslint-disable-next-line
+    window.navigator.msSaveOrOpenBlob(file, filename);
+  else { // Others
+    // eslint-disable-next-line
+    const a = document.createElement('a')
+    // eslint-disable-next-line
+    const url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    // eslint-disable-next-line
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+      // eslint-disable-next-line
+      document.body.removeChild(a);
+      // eslint-disable-next-line
+      window.URL.revokeObjectURL(url);
+    }, 0);
+  }
 }
