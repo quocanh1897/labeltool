167a515f5ad7e8fde79c0a6156e0cc7b3dbcbd48
Trung Trinh <trinhtrung96@gmail.com>
2018-01-28 12:39:09 +0700

    Fix

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 export function getSelectionText() {
-  // eslint-disable-next-line
   const selObj = window.getSelection();
   try {
     const selRange = selObj.getRangeAt(0);
@@ -26,26 +25,18 @@ export function checkParentRelation(parentNode, childNode) {
   return parentNode.compareDocumentPosition(childNode) % 16;
 }
 export function download(data, filename, type) {
-  // eslint-disable-next-line
   const file = new Blob([data], { type });
-  // eslint-disable-next-line
-  if (window.navigator.msSaveOrOpenBlob) // IE10+
-    // eslint-disable-next-line
+  if (window.navigator.msSaveOrOpenBlob) { // IE10+
     window.navigator.msSaveOrOpenBlob(file, filename);
-  else { // Others
-    // eslint-disable-next-line
-    const a = document.createElement('a')
-    // eslint-disable-next-line
+  } else { // Others
+    const a = document.createElement('a');
     const url = URL.createObjectURL(file);
     a.href = url;
     a.download = filename;
-    // eslint-disable-next-line
     document.body.appendChild(a);
     a.click();
     setTimeout(() => {
-      // eslint-disable-next-line
       document.body.removeChild(a);
-      // eslint-disable-next-line
       window.URL.revokeObjectURL(url);
     }, 0);
   }
